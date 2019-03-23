383ce5b60b287712083402b8142b9cb5d6f7f1bf
Ray Lee <raylee971227@gmail.com>
2019-03-22 23:54:26 -0400

    Sequelize/Postgres up and running. Need to fix bugs in model definitions.

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,8 +1,6 @@
 const db = require('./db');
 
 // register models
-// require('./models/Albums.js');
-// require('./models/Comments.js');
-require('./models/Students.js');
 
+require('./models/Students.js')
 module.exports = db;
