c7b8d32c2267b9db26224107fc85faffa34edf3f
Ray Lee <raylee971227@gmail.com>
2019-03-27 16:16:52 -0400

    changed nodemon to node bc it was annoying af

--- src/index.js
+++ src/index.js
@@ -4,7 +4,3 @@ import ReactDOM from 'react-dom';
 import Main from './client/components/Main'
 
 ReactDOM.render(<Main />, document.getElementById('Main'));
-<<<<<<< HEAD
-=======
-
->>>>>>> 655f19bdfe473dfe3c952a4c44ed41631d76bd32
