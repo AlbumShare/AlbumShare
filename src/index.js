57c9287f2c95503f7c6dc2356f0f2e115f51270c
Ray Lee <raylee971227@gmail.com>
2019-03-27 15:51:49 -0400

    fixed merge conflicts

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom';
 
 import Main from './client/components/Main'
 
-<<<<<<< HEAD
-ReactDOM.render(<Main />, document.getElementById('root'));
-=======
 ReactDOM.render(<Main />, document.getElementById('main'));
 
->>>>>>> 692c5ba87b249b523280a559a1b69f02b9fc31e8
