c69e78aea76799df7debe556bc7a1ab184c242c4
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-04-14 15:28:10 -0400

    Nav update

--- src/client/components/Search.js
+++ src/client/components/Search.js
@@ -1,37 +1,29 @@
 
 
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route,Switch} from 'react-router-dom';
 
-import logo1 from './img/Logo1.svg';
-import style from '../public/styles.css';
-
-import Login from './Main_Pages/Login';
-import SignUp from './Main_Pages/SignUp';
+import navigation from './Search_Pages/nav'
 
 import './css/Main.css'
 export default class Home extends Component {
   render() {
 
     return (
-      <Router>
-
       <div className="App">
-        <header className="App-header">
-
-        <div className="Iconhold">
-          <Link to="/"><img id="icon" src={logo1} alt="" /></Link>
-          <p id="logo_disc">Make sharing meaningful</p>  
-            this is gonna be search page
+        <div id = "navbar">
+        <Router>
+          <Switch>
+            <Route path="/" component={navigation}/>
+          </Switch>
+        </Router>
         </div>
+        
+        <div id = "feed">
+            bunch of picsk
 
-
-          
-        </header>
-      
-
+        </div>
       </div>
-      </Router>
 
     );
   }
---
+++ src/client/components/Search_Pages/nav.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
+
+
+class navigation extends React.Component{
+    render() {
+        return (
+        <div>
+                <h3>Search bar</h3>
+                <input type="text"/>
+                <button></button>
+        </div>
+        );
+    }
+}
+export default navigation
