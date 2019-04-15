9e4af7ce94dbc1465d3086c181c897d409a92064
Iris Huang <iris@iriss-mbp.home>
2019-04-15 10:24:57 -0400

    fixed the cookie bug

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,7 @@ const db = require('./db');
 const sessionStore = new SequelizeStore({db});
 const morgan = require('morgan');
 const passport = require('passport');
+const cookieParser = require ('cookie-parser');
 
 
 // register passport
@@ -49,33 +50,36 @@ app.use(volleyball);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// cookie parsering middleware
+app.use(cookieParser());
+
 // express-session: used for keep user logging in
-// app.use(session({
-//   // secret: 'true',
-//   secret: 'secret_string',
-//   store: sessionStore,
-//   resave: false,
-//   saveUninitialized: true
-// }))
+app.use(session({
+  // secret: 'true',
+  secret: 'secret_string',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: true
+}))
 
 // passport middleware: used for auth user
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// // check if client sent cookie
-// app.use(function(req, res, next) {
-//   console.log('session id is: , ', req.session.id)
-//   var cookie = req.cookies.cart // THIS IS UNDEFINED!
-//   if (cookie === undefined) {
-//     // no: set a new cookie
-//     res.cookie('cart', '', {maxAge: 900000, httpOnly: true})
-//     console.log('cookie created successfully')
-//   } else {
-//     // yes, cookie was already present
-//     console.log('cookie exists', cookie)
-//   }
-//   next()
-// });
+// check if client sent cookie
+app.use(function(req, res, next) {
+  console.log('session id is: , ', req.session.id)
+  var cookie = req.cookies.cart // THIS IS UNDEFINED!
+  if (cookie === undefined) {
+    // no: set a new cookie
+    res.cookie('cart', '', {maxAge: 900000, httpOnly: true})
+    console.log('cookie created successfully')
+  } else {
+    // yes, cookie was already present
+    console.log('cookie exists', cookie)
+  }
+  next()
+});
 
 // body parsing middleware
 app.use(bodyparser.json());
