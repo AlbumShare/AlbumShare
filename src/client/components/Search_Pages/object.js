5ecddc11817b668c4ddfd244d9a10797f2e7d82d
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-04-14 22:05:04 -0400

    Update object.js

--- src/client/components/Search_Pages/object.js
+++ src/client/components/Search_Pages/object.js
@@ -2,16 +2,23 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import '../css/Search.css'
 import axios from 'axios';
-import navigation from './nav.js'
+import navigation from './nav'
 
 
 class objects extends React.Component{
-
-   
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            searching: navigation.state.nav
+        };
+        
+      }
+    
     render() {
         return (
         <div  id = "feed" >
-            filler text
+
         </div>
         );
     }
