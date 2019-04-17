bf4d7530d8873887b370d78c2a0c8d9d260615d5
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-04-17 15:04:23 -0400

    Something

--- src/server/api/auth.js
+++ src/server/api/auth.js
@@ -8,25 +8,30 @@ router.post('/login', async (req, res, next) => {
     const user = await Users.findOne({where: {email: req.body.email}});
     if(!user) {
       res.status(401).send('Wrong username and/or password');
-      alert('Wrong username and/or password');
 
     }
     else if(!user.passwordIsCorrect(req.body.password)) {
       res.status(res.status(401).send('Wrong username and/or password'));
-      alert('Wrong username and/or password');
 
     }
     else {
-      console.log('log in success!');
-      alert('log in success!'); 
+      console.log('log in success! ');
+
 
       // req.logIn(user, err => (err ? next(err) : res.json(user)));
       req.logIn(user, err => {
+        
+        console.log(err);
+
         if(err) return next(err);
+        
+
         console.log(req.user);
       })
+
     }
   } catch (error) {
+
     next(error);
   }
   
